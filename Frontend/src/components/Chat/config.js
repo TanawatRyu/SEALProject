fbea43e5f3283ef2ffbda53ec80ff62dba142f5f
TanawatRyu <gunmag3307985@hotmail.com>
2018-12-22 19:43:50 +0700

    [ADD] master : create config use token router to google assistant

---
+++ Frontend/src/components/Chat/config.js
@@ -0,0 +1,13 @@
+export default {
+    app: {
+        token: "ba6803243f78473e8b73c90d6afc2ef1", // <- enter your token here
+        googleIt: true // <- ask users to google their request, in case of input.unknown action
+    },
+    locale: {
+        strings: {
+            welcomeTitle: "Hello, ask something to get started.",
+            welcomeDescription: `You can type "Hello" for example.`,
+            queryTitle: "Ask me something...",
+        }
+    }
+}
