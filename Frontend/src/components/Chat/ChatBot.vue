2e929e68983c568942652dd2282a01b29d329925
TanawatRyu <gunmag3307985@hotmail.com>
2018-12-22 15:36:09 +0700

    [ADD] master : create chatbot

---
+++ Frontend/src/components/Chat/ChatBot.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout justify-center align-center>
+          <v-flex text-xs-center>
+              <p>Hello</p>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
