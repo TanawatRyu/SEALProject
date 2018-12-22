cb83f3ea6a5ab7672a715563429f4b95fb9c910b
TanawatRyu <gunmag3307985@hotmail.com>
2018-12-22 19:42:04 +0700

    [EDIT] master : edit page chatbot ui

--- Frontend/src/components/Chat/ChatBot.vue
+++ Frontend/src/components/Chat/ChatBot.vue
@@ -1,13 +1,76 @@
 <template>
   <v-app>
     <v-content>
-      <v-container fluid fill-height>
         <v-layout justify-center align-center>
           <v-flex text-xs-center>
-              <p>Hello</p>
+              <div class="query">
+                <input :aria-label="config.locale.strings.queryTitle" autocomplete="off" v-model="query" class="queryform" @keyup.enter="submit()" :placeholder="config.locale.strings.queryTitle" autofocus type="text">
+              </div>
+                <div v-if="answers.length == 0">
+                    <div class="headline">
+                        <div class="material-icons up">arrow_upward</div>
+                        <br>
+                        <br>
+                            {{config.locale.strings.welcomeTitle}}
+
+                        <p class="body2">{{config.locale.strings.welcomeDescription}}</p>
+                    </div>
+                </div>    
           </v-flex>
         </v-layout>
-      </v-container>
     </v-content>
   </v-app>
 </template>
+
+<script>
+import { ApiAiClient } from 'api-ai-javascript'
+import config from './config'
+
+const client = new ApiAiClient({accessToken: config.app.token})
+
+export default {
+    name:'ChatBot',
+    data: function(){
+    return {
+        answers: [],
+        query: '',
+        speech: config.locale.strings.voiceTitle,
+        micro: false,
+        muted: config.app.muted,
+        online: navigator.onLine,
+        config
+        }
+    },
+    methods: {
+        submit(){
+
+        }
+    }
+
+}
+</script>
+<style>
+#app {
+  background-color: #e0e0e0;
+}
+.query{
+    height: 50px;
+    width: 100%;
+    background-color:white;
+    margin: auto;
+}
+.queryform{
+  margin-top: 10px;   
+  font-size: 20px;
+  text-align: center;
+}
+.headline{
+  font-weight: bold;
+  text-align: center;
+  margin-top: 200px;
+
+}
+.material-icons{
+
+}
+</style>
